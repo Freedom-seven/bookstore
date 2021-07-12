11dbc7ee5e84bd925371a519fcb402956a8cf3ba
FREEDOM <freewhynane62@gmail.com>
2021-07-12 08:50:41 +0000

    add useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 
-const App = () => {
+const App = (props) => {
+  const [bookstore, setBookstore] = useState({
+    bookStore: [
+      {
+        title: "Programmer",
+        author: "Freedom",
+        description: "Discover your hidden Talent"
+      },
+      {
+        title: "Coding",
+        author: "Randy",
+        description: "Lrean to CODE the right way"
+      },
+      {
+        title: "LIFE",
+        author: "Orlando",
+        description: "LIFE as a DEVELOPER"
+      }
+    ]
+  })
   return (
     <div>
       <h1>BOOKSTORE</h1>
